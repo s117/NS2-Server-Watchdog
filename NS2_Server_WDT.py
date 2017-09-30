3ddeedc90f29c9097032c9d60a073740619cfc63
John <admin@0x10c.pw>
2017-09-30 13:53:53 +0800

    several minor improvement

--- NS2_Server_WDT.py
+++ NS2_Server_WDT.py
@@ -15,7 +15,7 @@ ExitFlag = False
 
 
 class Logger:
-    __TIME_LABEL_PATTERN = '%Y-%m-%d %H:%M:%S'
+    __TIME_LABEL_PATTERN = '%m/%d/%y-%H:%M:%S'
 
     def __init__(self):
         raise NotImplementedError("This class should never be instantiated.")
@@ -23,33 +23,43 @@ class Logger:
     @staticmethod
     def debug(text):
         if VERBOSE_LEVEL >= 2:
-            print time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())) + " [DEBUG]   - " + str(text)
+            sys.stdout.write(
+                "[%s] <%s>: %s\n" % (time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())),
+                                     "DEBUG", str(text)))
 
     @staticmethod
     def verbose(text):
         if VERBOSE_LEVEL >= 1:
-            print time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())) + " [VERBOSE] - " + str(text)
+            sys.stdout.write(
+                "[%s] <%s>: %s\n" % (time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())),
+                                     "VERBOSE", str(text)))
 
     @staticmethod
     def info(text):
-        print time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())) + " [INFO]    - " + str(text)
+        sys.stdout.write(
+            "[%s] <%s>: %s\n" % (time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())),
+                                 "INFO", str(text)))
 
     @staticmethod
     def warn(text):
-        print time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())) + " [WARN]    - " + str(text)
+        sys.stderr.write(
+            "[%s] <%s>: %s\n" % (time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())),
+                                 "WARN", str(text)))
 
     @staticmethod
     def fatal(text, exitcode=-1):
-        print time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())) + " [FATAL]   - " + str(text)
-        print time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(
-            time.time())) + " [FATAL]   - " + "Program terminated, exit code: %d." % exitcode
+        sys.stderr.write(
+            "[%s] <%s>: %s\n" % (time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())),
+                                 "FATAL", str(text)))
+        sys.stderr.write(
+            "[%s] <%s>: %s\n" % (time.strftime(Logger.__TIME_LABEL_PATTERN, time.localtime(time.time())),
+                                 "FATAL", ("Program terminated, exit code: %d." % exitcode)))
         exit(exitcode)
 
 
 class ConfigManager:
-    _manager = None
-    _CONFIG_FILENAME = "config.json"
-    _DEFAULT_CONFIG = {
+    __CONFIG_FILENAME = "config.json"
+    __DEFAULT_CONFIG = {
         # Monitoring interval in second.
         "monitor_interval": 1,
 
@@ -82,13 +92,13 @@ class ConfigManager:
         # The name of the server's executable file.
         "server_executable_image": "Server.exe",
 
-        # Path to server config dir.
+        # Path to server config dir. If you use the relative path, it should be relative to the server's root
         "server_config_cfg_dir": "./configs/ns2server_configs",
 
-        # Path to server log dir.
+        # Path to server log dir. If you use the relative path, it should be relative to the server's root
         "server_config_log_dir": "./configs/ns2server_logs",
 
-        # Path to server mod storage dir.
+        # Path to server mod storage dir. If you use the relative path, it should be relative to the server's root
         "server_config_mod_dir": "./configs/ns2server_mods",
 
         # Additional launch option.
@@ -112,36 +122,39 @@ class ConfigManager:
 
     @staticmethod
     def save_config():
-        with open(ConfigManager._CONFIG_FILENAME, 'w') as f:
+        with open(ConfigManager.__CONFIG_FILENAME, 'w') as f:
             f.write(json.dumps(ConfigManager.__config, indent=4, sort_keys=True))
-        Logger.info("Flushed config to '%s'" % ConfigManager._CONFIG_FILENAME)
+        Logger.info("Flushed config to '%s'" % ConfigManager.__CONFIG_FILENAME)
 
     @staticmethod
     def load_config():
         ConfigManager.__config = {}
-        if os.path.exists(ConfigManager._CONFIG_FILENAME):
-            Logger.info("Loading config from file '%s'" % ConfigManager._CONFIG_FILENAME)
-            with open(ConfigManager._CONFIG_FILENAME) as json_file:
+        if os.path.exists(ConfigManager.__CONFIG_FILENAME):
+            Logger.info("Loading config from file '%s'" % ConfigManager.__CONFIG_FILENAME)
+            with open(ConfigManager.__CONFIG_FILENAME) as json_file:
                 json_data = json.load(json_file)
-                for i, v in ConfigManager._DEFAULT_CONFIG.items():
+                for i, v in ConfigManager.__DEFAULT_CONFIG.items():
                     if i in json_data:
                         ConfigManager.__config[i] = json_data[i]
                     else:
-                        ConfigManager.__config[i] = ConfigManager._DEFAULT_CONFIG[i]
+                        ConfigManager.__config[i] = ConfigManager.__DEFAULT_CONFIG[i]
         else:
-            Logger.info("File '%s' not found, a new one will be created" % ConfigManager._CONFIG_FILENAME)
-            ConfigManager.__config = ConfigManager._DEFAULT_CONFIG
+            Logger.info("File '%s' not found, a new one will be created" % ConfigManager.__CONFIG_FILENAME)
+            ConfigManager.__config = ConfigManager.__DEFAULT_CONFIG
         ConfigManager.save_config()
         Logger.info("Config loaded")
 
 
 class ServerProcessHandler:
     __WAIT_TIME_BEFORE_FORCE_KILL = 5
-    __WAIT_TIME_BEFORE_GIVE_UP = 5
+    __WAIT_TIME_BEFORE_GIVE_UP = 60
 
     def __init__(self):
         if not os.path.isdir(ConfigManager.get_config("server_path")):
             Logger.fatal("The root of NS2Server ('%s') does not exist" % ConfigManager.get_config("server_path"))
+        if not os.path.isabs(ConfigManager.get_config("server_path")):
+            Logger.warn("You are using relative path '%s' to specify the server root" % (
+                ConfigManager.get_config("server_path")))
 
         prev_dir = os.getcwd()
         os.chdir(ConfigManager.get_config("server_path"))
@@ -152,7 +165,7 @@ class ServerProcessHandler:
             vd = ConfigManager.get_config(kd)
             if not os.path.isdir(vd):
                 Logger.fatal(
-                    "Fail to start server, because directory '%s' does not exist (value of config '%s')" % (vd, kd))
+                    "Fail to start server, because directory '%s' does not exist (value of '%s')" % (vd, kd))
             if not os.path.isabs(vd):
                 Logger.warn("You are using relative path '%s' for config '%s')" % (vd, kd))
         executable_path = ConfigManager.get_config("server_path") + "/" + ConfigManager.get_config(
@@ -201,25 +214,26 @@ class ServerProcessHandler:
                     cmdline = cmdline + p + " "
 
                 self.__process = Popen(cmdline,
+                                       close_fds=True,
                                        cwd=ConfigManager.get_config("server_path"),
                                        creationflags=CREATE_NEW_CONSOLE)
 
                 self.__pid = self.__process.pid
                 self.__ps = psutil.Process(pid=self.__pid)
                 self.__ps_cmdline = ""
+                self.__ps_create_time = self.__ps.create_time()
 
                 Logger.debug("Process's cmdline: %s" % str(self.__ps.cmdline()))
 
                 for i in self.__ps.cmdline():
                     self.__ps_cmdline = self.__ps_cmdline + i + " "
-                self.__ps_create_time = self.__ps.create_time()
 
             except psutil.NoSuchProcess:
                 self.__pid = -1
                 self.__process = None
                 self.__ps = None
                 self.__ps_cmdline = None
-                Logger.fatal("Fail to start the server, please check the setting and the server's integrity")
+                Logger.fatal("Fail to start the server, please check the setting or the server's integrity")
             except psutil.AccessDenied:
                 Logger.fatal("Access denied when try to control the server process (pid %d)" % self.__pid)
 
@@ -231,19 +245,19 @@ class ServerProcessHandler:
                 Logger.info("Try to stop server process (pid %d) by terminate()" % self.__pid)
                 self.__ps.terminate()
                 try:
-                    self.__ps.wait(15)
+                    self.__ps.wait(self.__WAIT_TIME_BEFORE_FORCE_KILL)
                     Logger.info("Server process (pid %d) get terminated" % self.__pid)
                 except psutil.TimeoutExpired:
                     Logger.warn("Fail to stop server process (pid %d) by terminate(), trying kill()" % self.__pid)
                     try:
                         self.__ps.kill()
-                        self.__ps.wait(15)
+                        self.__ps.wait(self.__WAIT_TIME_BEFORE_GIVE_UP)
                         Logger.info("Server process (pid %d) get killed" % self.__pid)
                     except psutil.TimeoutExpired:
                         Logger.fatal("Fail to terminate server process (pid %d)" % self.__pid)
 
             except psutil.NoSuchProcess:
-                pass
+                Logger.info("Server process (pid %s) already get stopped" % self.__pid)
             except psutil.AccessDenied:
                 Logger.fatal("Access denied when try to control the server process (pid %d)" % self.__pid)
 
@@ -282,8 +296,9 @@ class ServerProcessHandler:
                     "create_time": self.__ps_create_time,
                 }
 
-    def __force_update_ping_mode_data(self):
-        # The ping_mode's record need to be removed before start because otherwise
+    @staticmethod
+    def __force_update_ping_mode_data():
+        # The ping_mode's record need to be updated before start because otherwise
         # the launching progress will be disturbed by the lua engine check.
         DEFAULT_PING_MODE_NAME = "server_modding_ping.txt"
         ABS_SERVER_CONFIG_DIR = convert_rel_path_to_abs_path(ConfigManager.get_config("server_path"),
@@ -298,9 +313,10 @@ class ServerProcessHandler:
         except IOError:
             Logger.fatal("Fail to force update the ping_mode record, check user permission")
         else:
-            Logger.verbose("Force updated the ping_mod record, value: %s" % st)
+            Logger.verbose("Force updated the ping_mod record, value: '%s'" % st)
 
-    def __archive_log_and_dmp(self):
+    @staticmethod
+    def __archive_log_and_dmp():
         DEFAULT_LOG_FILE_NAME = "log-Server.txt"
         DEFAULT_DUMP_FILE_NAME = "lastcrash.dmp"
 
@@ -312,18 +328,18 @@ class ServerProcessHandler:
 
         try:
             if os.path.exists(DEFAULT_DUMP_FILE_NAME):
+                os.rename(DEFAULT_DUMP_FILE_NAME, time_label + "_" + DEFAULT_DUMP_FILE_NAME)
                 Logger.verbose("Archived the crash dump: '%s' -> '%s'" % (
                     DEFAULT_DUMP_FILE_NAME, time_label + "_" + DEFAULT_DUMP_FILE_NAME))
-                os.rename(DEFAULT_DUMP_FILE_NAME, time_label + "_" + DEFAULT_DUMP_FILE_NAME)
-        except Exception:
+        except IOError:
             Logger.fatal("Fail to archive the crash dump file. (Maybe the last server process is still running?)")
 
         try:
             if os.path.exists(DEFAULT_LOG_FILE_NAME):
+                os.rename(DEFAULT_LOG_FILE_NAME, time_label + "_" + DEFAULT_LOG_FILE_NAME)
                 Logger.verbose("Archived the server log: '%s' -> '%s'" % (
                     DEFAULT_LOG_FILE_NAME, time_label + "_" + DEFAULT_LOG_FILE_NAME))
-                os.rename(DEFAULT_LOG_FILE_NAME, time_label + "_" + DEFAULT_LOG_FILE_NAME)
-        except Exception:
+        except IOError:
             Logger.fatal("Fail to archive the server log file. (Maybe the last server process is still running?)")
         os.chdir(prev_dir)
 
@@ -349,6 +365,8 @@ class ServerWatchDog:
                                                              ConfigManager.get_config("server_config_cfg_dir"))
         self.__abspath_ping_mod_output = abspath_server_config + "/server_modding_ping.txt"
         self.__lua_engine_no_response_threshold = ConfigManager.get_config("lua_engine_no_response_threshold")
+        self.__ping_mod_record_pattern = ConfigManager.get_config("format_ping_mod_record")
+        self.__ping_mod_output_invalid_cnt = 0
 
     def run_server(self):
         sleep_sec = ConfigManager.get_config("monitor_interval")
@@ -377,7 +395,7 @@ class ServerWatchDog:
             return False
 
         if time.time() >= self.__next_daily_restart_trigger_time:
-            Logger.info("Daily restart: Now is the time to restart")
+            Logger.info("Daily restart: now is the time to restart")
             self.__next_daily_restart_trigger_time = self.__calc_next_daily_restart_trigger_timestamp()
             process_info = self.__server.get_info()
             if process_info is not None:
@@ -389,38 +407,55 @@ class ServerWatchDog:
                     Logger.info("Daily restart: server will get restarted (vms/%d >= threshold/%d)" % (
                         process_info["vms"], self.__daily_restart_vms_threshold))
                     return True
-            else:  # not now
+            else:
                 Logger.info("Daily restart: server will get restarted (server process stopped)")
                 return True
-        else:
+        else:  # not now
             return False
 
     def __is_server_lua_engine_dead(self):
         if not self.__is_check_lua_engine_status:
             return False
 
-        if not os.path.isfile(self.__abspath_ping_mod_output):
-            Logger.warn(
-                "Lua engine check: Fail to find ping_mod's output at '%s'. Assume engine is well" % (
-                    self.__abspath_ping_mod_output))
-            return False
-
         st = "uninitialized"
         try:
             with open(self.__abspath_ping_mod_output, 'r') as f:
                 st = f.readline()
-                last_update_time = datetime.datetime.strptime(st, ConfigManager.get_config("format_ping_mod_record"))
-                last_update_timestamp = time.mktime(last_update_time.timetuple())
+            last_update_time = datetime.datetime.strptime(st, self.__ping_mod_record_pattern)
+            last_update_timestamp = time.mktime(last_update_time.timetuple())
+            self.__ping_mod_output_invalid_cnt = 0
         except IOError:
-            Logger.warn(
-                "Lua engine check: Fail to open ping_mod's output '%s'. Assume engine is good" % (
-                    self.__abspath_ping_mod_output))
-            return False
+            self.__ping_mod_output_invalid_cnt = self.__ping_mod_output_invalid_cnt + 1
+            if self.__ping_mod_output_invalid_cnt < self.__lua_engine_no_response_threshold:
+                Logger.warn(
+                    ("Lua engine check: Fail to open ping_mod's output: '%s' (%d/%d). " +
+                     "Assume engine is good") % (
+                        self.__abspath_ping_mod_output,
+                        self.__ping_mod_output_invalid_cnt, self.__lua_engine_no_response_threshold))
+                return False
+            else:
+                Logger.warn(
+                    ("Lua engine check: Fail to open ping_mod's output: '%s' (%d/%d). " +
+                     "Assume engine is down") % (
+                        self.__abspath_ping_mod_output,
+                        self.__ping_mod_output_invalid_cnt, self.__lua_engine_no_response_threshold))
+                return True
         except ValueError:
-            Logger.warn(
-                "Lua engine check: Fail to parse the %s with pattern %s in ping_mod's record. Assume engine is down" % (
-                    st, ConfigManager.get_config("format_ping_mod_record")))
-            return True
+            self.__ping_mod_output_invalid_cnt = self.__ping_mod_output_invalid_cnt + 1
+            if self.__ping_mod_output_invalid_cnt < self.__lua_engine_no_response_threshold:
+                Logger.warn(
+                    ("Lua engine check: Fail to parse the ping_mod's record '%s' with pattern %s (%d/%d). " +
+                     "Assume engine is good") % (
+                        st, self.__ping_mod_record_pattern,
+                        self.__ping_mod_output_invalid_cnt, self.__lua_engine_no_response_threshold))
+                return False
+            else:
+                Logger.warn(
+                    ("Lua engine check: Fail to parse the ping_mod's record '%s' with pattern %s (%d/%d). " +
+                     "Assume engine is down") % (
+                        st, self.__ping_mod_record_pattern,
+                        self.__ping_mod_output_invalid_cnt, self.__lua_engine_no_response_threshold))
+                return True
         else:
             engine_frozen_time = int(time.time() - last_update_timestamp)
             if engine_frozen_time > self.__lua_engine_no_response_threshold:
